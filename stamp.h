c4a9c22c80ff4a84ea3850ba502ce8e98c52ea1d
hudhuud <118181770+hudhuud@users.noreply.github.com>
2024-03-19 23:27:22 +0300

    Create stamp.h

---
+++ stamp.h
@@ -0,0 +1,35 @@
+#ifndef STAMP_H
+#define STAMP_H
+
+#define MAX_STAMPS 100
+
+struct Stamp {
+    int index;
+    char name[50];
+    char country[50];
+    int year;
+    int albumNumber;
+    int price;
+};
+
+int readStampsFromFile(const char *fileName, struct Stamp stamps[], int *stampCount);
+
+int writeSortedStampsToFile(const char *fileName, struct Stamp stamps[], int stampCount);
+
+void displayMenu(struct Stamp stamps[], int *stampCount);
+
+void addStampMenu(struct Stamp stamps[], int *stampCount);
+
+void deleteStampMenu(struct Stamp stamps[], int *stampCount);
+
+void editStampMenu(struct Stamp stamps[], int *stampCount);
+
+void printAllStampsMenu(struct Stamp stamps[], int *stampCount);
+
+void query1Menu(struct Stamp stamps[], int *stampCount);
+
+void query2Menu(struct Stamp stamps[], int *stampCount);
+
+void exitMenu(struct Stamp stamps[], int *stampCount);
+
+#endif 
