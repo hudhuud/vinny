b5dd1923e16f054ab853b286b1d0323ef6d9417b
hudhuud <118181770+hudhuud@users.noreply.github.com>
2024-03-26 18:42:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,24 @@ int main(void) {
     struct Stamp stamps[100];
     int stampCount = 0;
 
-    if (!readStampsFromFile("in.txt", stamps, &stampCount)) {
-        printf("Ошибка при чтении марок из файла.\n");
+    // Считываем данные о марках из файлов
+    if (!readStampsFromFile("additional_info.txt", stamps, &stampCount)) {
+        printf("Ошибка при чтении марок из файла in.txt.\n");
         return 1;
     }
 
+    // Считываем дополнительные данные о марках из файла stamps_details.txt
+    if (!readStampsDetailsFromFile("stamps_details.txt", stamps, &stampCount)) {
+        printf("Ошибка при чтении дополнительных данных о марках из файла stamps_details.txt.\n");
+        return 1;
+    }
+
+    // Отображаем меню для работы с марками
     displayMenu(stamps, &stampCount);
 
+    // Записываем отсортированные марки в файл
     if (!writeSortedStampsToFile("out.txt", stamps, stampCount)) {
-        printf("Ошибка при записи марок в файл.\n");
+        printf("Ошибка при записи марок в файл out.txt.\n");
         return 1;
     }
 
