76a11dffca592404854b5290357a80d97b74730c
hudhuud <118181770+hudhuud@users.noreply.github.com>
2024-03-26 18:43:39 +0300

    Update stamp.h

--- stamp.h
+++ stamp.h
@@ -2,10 +2,13 @@
 #define STAMP_H
 
 #define MAX_STAMPS 100
+#define MAX_NAME_LENGTH 50
+#define MAX_COUNTRY_LENGTH 50
+
 struct Stamp {
     int index;
-    char name[50];
-    char country[50];
+    char name[MAX_NAME_LENGTH];
+    char country[MAX_COUNTRY_LENGTH];
     int year;
     int albumNumber;
     int price;
@@ -14,6 +17,8 @@ struct Stamp {
 int readStampsFromFile(const char *fileName, struct Stamp stamps[], int *stampCount);
 
 int writeSortedStampsToFile(const char *fileName, struct Stamp stamps[], int stampCount);
+
+int readStampsDetailsFromFile(const char *fileName, struct Stamp stamps[], int *stampCount);
 
 void displayMenu(struct Stamp stamps[], int *stampCount);
 
@@ -31,4 +36,4 @@ void query2Menu(struct Stamp stamps[], int *stampCount);
 
 void exitMenu(struct Stamp stamps[], int *stampCount);
 
-#endif 
+#endif
